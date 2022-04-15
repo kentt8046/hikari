d8d903ad2f3539c1b71d3b52051bddf933500720
kentt8046 <kentt8046@gmail.com>
2022-04-15 20:26:10 +0900

    chore(hikari): Update example

--- example/basic.dart
+++ example/basic.dart
@@ -1,9 +1,14 @@
 import 'package:hikari/hikari.dart';
 
 Future<void> main() async {
-  await Server("0.0.0.0", 3000, "0.0.1").serve((req) {
-    return Response.ok(body: "Hello World");
-  }, onStarted: (server) {
-    print("listening on http://localhost:${server.port}");
-  });
+  final router = Router()
+    ..get("/", (req) => Response.ok(body: "Hello World"))
+    ..get("/echo", (req) => Response.ok(body: req.query["echo"] ?? "empty"));
+
+  await Server("0.0.0.0", 3000, "0.0.1").serve(
+    router.handle(),
+    onStarted: (server) {
+      print("listening on http://localhost:${server.port}");
+    },
+  );
 }
