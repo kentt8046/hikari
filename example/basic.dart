9aebe9990fbc693ac168772d9654b0a790cb393b
kentt8046 <kentt8046@gmail.com>
2022-04-15 21:08:54 +0900

    chore(hikari): Update example

--- example/basic.dart
+++ example/basic.dart
@@ -5,10 +5,30 @@ Future<void> main() async {
     ..get("/", (req) => Response.ok(body: "Hello World"))
     ..get("/echo", (req) => Response.ok(body: req.query["echo"] ?? "empty"));
 
+  final handler = logRequests().handle(router.handle());
+
   await Server("0.0.0.0", 3000, "0.0.1").serve(
-    router.handle(),
+    handler,
     onStarted: (server) {
       print("listening on http://localhost:${server.port}");
     },
   );
+}
+
+Middleware logRequests() {
+  return (next) => (req) async {
+        print("${req.method} ${req.uri} ==>");
+
+        Response? res;
+        try {
+          res = await next(req);
+
+          return res;
+        } catch (_) {
+          res = Response.internalServerError();
+          rethrow;
+        } finally {
+          print("${req.method} ${req.uri} <== ${res?.status ?? 404}");
+        }
+      };
 }
