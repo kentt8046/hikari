4547d4bf05e80abaa173c2d48e2dbb791681283b
kentt8046 <kentt8046@gmail.com>
2022-05-13 17:31:55 +0900

    chore(hikari): Change to work with v2.17

--- tools/test_coverage.sh
+++ tools/test_coverage.sh
@@ -4,6 +4,6 @@ coverage_dir=".coverage"
 
 rm -fr $coverage_dir/
 dart test --coverage=$coverage_dir/
-dart run coverage:format_coverage --lcov --in=$coverage_dir --out=$coverage_dir/lcov.info --packages=.packages --report-on=lib
+dart run coverage:format_coverage --lcov --in=$coverage_dir --out=$coverage_dir/lcov.info --report-on=lib
 genhtml $coverage_dir/lcov.info -o $coverage_dir/html
 open $coverage_dir/html/index.html
